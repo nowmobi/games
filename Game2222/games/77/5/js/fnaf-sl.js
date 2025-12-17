62e53e87eaa5b50ecfe75ca92490b8cd2efc8d3f
nowmobi <nowmobi@163.com>
2025-12-17 12:03:04 +0800

    Remove Game2222 games 74 and 77 assets

--- Game2222/games/77/5/js/fnaf-sl.js
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:0ce63692948b3f6ccb19f56034df519384d14b8cf06e7190e24e9bbef0986974
-size 932071
--- Game2222/games/77/5/js/octet-stream.js
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:ce67986b7f653d290945a73cdb69f1b82ddd4c48761ccaa26c08ea7bf1725644
-size 54865994
