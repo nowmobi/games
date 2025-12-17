ff9ed846c26f4cc8089a79aa7b76ca92f43db001
nowmobi <nowmobi@163.com>
2025-12-17 10:08:35 +0800

    Update Game2222: remove game 24 assets

--- Game2222/games/24/patch/js/poki-sdk-v2.2-core.js
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:7d35532c3b36c48cf78ad42571b49679fdaea26a7eea70655e80375b5c930554
-size 258421
--- Game2222/games/24/patch/js/poki-sdk-v2.js
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:e249569a324f2413ad88df22168d1ebe7867872d8681204e4aadfde301398a56
-size 5297
